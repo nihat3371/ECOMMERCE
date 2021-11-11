d906de11c495f5557d4a167db7b8464c7f3683ce
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2021-11-11 14:52:22 +0300

    commit on 'master'

---
+++ src/components/Slider.jsx
@@ -0,0 +1,131 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100%;
+    height:100vh;
+    display:flex;
+    position: relative:
+    overflow: hidden;
+`
+
+const Arrow = styled.div`
+    width:50px;
+    height:50px;
+    background-color: #fff7f7;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props=> props.direction === "left" && "10px"};
+    right: ${props=> props.direction === "right" && "10px"};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+    z-index: 2;
+`;
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transform:translateX(0vw)
+`;
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color:#${props=>props.bg} ; 
+    
+`;
+
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`;
+
+const Image = styled.img`
+    height: 80%;
+    margin-left : 250px;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`;
+
+const title = styled.h1`
+    font-size: 70px;
+`;
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 32px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`;
+
+export const Slider = () => {
+
+        const [slideIndex, setslideIndex] = useState(0);
+        const handleClick = (direction) =>{}
+
+    return (
+        <Container>
+            <Arrow direction="left" onClick={()=>handleClick("left")} >
+                <ArrowLeftOutlined/>
+            </Arrow >
+            <Wrapper>
+            <Slide bg = "f5fafd">
+
+                
+            <ImgContainer>
+            <Image src ="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+             </ImgContainer>
+                <InfoContainer>
+                    <title>SUMMER SALE</title>
+                    <Desc>DON'T COMPROMISE ON STLYE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                    <Button>SHOW NOW</Button>
+                    </InfoContainer>
+            </Slide>
+            <Slide bg = "fcf1ed">
+
+                
+            <ImgContainer>
+             <Image src ="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+              </ImgContainer>
+                <InfoContainer>
+                    <title>WINTER SALE</title>
+                    <Desc>DON'T COMPROMISE ON STLYE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                    <Button>SHOW NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            <Slide bg = "fbf0f4" >
+
+                
+<ImgContainer>
+<Image src ="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+</ImgContainer>
+<InfoContainer>
+    <title>POPULAR SALE</title>
+    <Desc>DON'T COMPROMISE ON STLYE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+    <Button>SHOW NOW</Button>
+</InfoContainer>
+</Slide>
+            </Wrapper>
+            <Arrow direction="right" onClick={()=>handleClick("right")} >
+                <ArrowRightOutlined/>
+            </Arrow>
+            
+        </Container>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STLYE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STLYE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STLYE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];

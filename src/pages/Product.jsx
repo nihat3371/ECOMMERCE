dffe0033f4bc3e6b325b24aa207fb09b472f9d3d
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2021-11-21 15:21:13 +0300

    commit on master

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@material-ui/icons";
 import styled from "styled-components"
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
@@ -72,7 +73,45 @@ const FilterSize = styled.select`
     padding: 5px;
 `;
 
-const FilterSizeOption = styled.option``;
+const FilterSizeOption = styled.option`
+    
+`;
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;  
+    align-items: center;
+    justify-content: center;
+    margin: 0px 10px;
+`;
+const Button = styled.button`
+    padding: 20px;
+    border: 2px solid black;
+    cursor: pointer;
+    background-color: white;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`;
+
 
 const Product = () => {
     return (
@@ -105,6 +144,14 @@ const Product = () => {
                             </FilterSize>
                         </Filter>
                     </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove/>
+                            <Amount>1</Amount>
+                            <Add/>
+                        </AmountContainer>
+                        <Button>ADD TO CARD</Button>
+                    </AddContainer>
                 </InfoContainer>
             </Wrapper>
             <Newsletter/>

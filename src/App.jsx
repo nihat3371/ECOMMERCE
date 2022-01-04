1f1840021bd5d0ecee3c900385ceb64b26446ee9
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-01-04 15:31:47 +0300

    commit on master

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./pages/Login";
 import Cart from "./components/Cart";
 
 const App = () => {
-  return <Register/>;
+  return <Home/>;
 };
 
-export default Cart ;
+export default App ;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,108 @@ const Container = styled.div`
 
 `;
 
+const Wrapper = styled.div`
+    padding: 20px;
+
+`;
+const Title = styled.h1`
+    font-weight:300;
+    text-align: center;
+`;
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`;
+
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${(props)=>props.type === "filled" && "none"};
+    background-color: ${(props) => props.type === "filled" ? "black" : "transparent"};
+    color: ${(props) => props.type === "filled" && "white"};
+    
+
+`;
+
+const TopTexts = styled.div`
+    
+`;
+
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+`;
+
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+    
+`;
+
+const Info = styled.div`
+    flex:3;
+`;
+
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+    flex:2;
+    display: flex;
+`;
+
+const Image = styled.img`
+    width: 200px;
+`;
+
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const ProductName = styled.span`
+    
+`;
+
+const ProductId = styled.div`
+    
+`;
+
+const ProductColor = styled.span`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+`;
+
+const ProductSize = styled.div`
+    
+`;
+
+const PriceDetail = styled.div`
+    flex:1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Summary = styled.div`
+    flex:1;
+`;
+
+const ProductAmountContainer = styled.div`
+
+`;
+
 const Cart = () => {
     return (
         <Container>
@@ -14,6 +116,35 @@ const Cart = () => {
             <Announcement/>
             <Wrapper>
                 <Title>YOUR BAG</Title>
+                <Top>
+                    <TopButton>CONTINUE SHOPPING</TopButton>
+                    <TopTexts>
+                        <TopText>Shopping Bag(2)</TopText>
+                        <TopText>Your Wishlist(0)</TopText>
+                    </TopTexts>
+                    <TopButton type="filled">CHECKOUT NOW</TopButton>
+                </Top>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src='https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'/>
+                                <Details>
+                                    <ProductName><b>Product:</b> JESSIE THUNDER SHOES</ProductName>
+                                    <ProductId><b>ID:</b> 9233567862</ProductId>
+                                    <ProductColor color="black"/>
+                                    <ProductSize><b>Size:</b> 37.5</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    
+                                </ProductAmountContainer>
+                            </PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>summary</Summary>
+                </Bottom>
             </Wrapper>
             <Footer/>
         </Container>
